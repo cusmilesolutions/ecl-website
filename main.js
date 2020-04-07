507b3bd06f8b708a01c433ac69fedcbce51324f5
Isaac Gyamfi <frimpongi50@gmail.com>
2020-04-07 17:18:32 +0000

    ready for production

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
   res.sendFile('index.html');
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () =>
   console.log(`Server is live on http://localhost:${PORT}`)
 );
